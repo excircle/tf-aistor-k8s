396656de6bcd7626289c27cd67f3521f175a7307
excircle <unixdisciple.us@gmail.com>
2024-11-21 12:16:56 -0800

    Ansible pre-deploy 16

--- ansible/update-ini.sh
+++ ansible/update-ini.sh
@@ -1,38 +1,41 @@
 USER=$1
+KEY_PATH=$2
+INI_PATH=$3
+NODES_FILE=$4
 
-touch /tmp/nodes.json
+touch $NODES_FILE
 
 aws ec2 describe-instances \
     --region us-west-2 \
     --filters "Name=tag:Name,Values=k8s-node*" "Name=instance-state-name,Values=running" \
     --query "Reservations[*].Instances[*].{Name:Tags[?Key=='Name']|[0].Value, PublicIpAddress:PublicIpAddress}" \
-    --output json | jq -r '[.[][]] | sort_by(.Name)' > /tmp/nodes.json
+    --output json | jq -r '[.[][]] | sort_by(.Name)' > $NODES_FILE
 
 # Create [all] Group
-echo -e "[all]" >> /tmp/project.ini
+echo -e "[all]" >> $INI_PATH
 
-for i in $(jq -r '.[].Name' /tmp/nodes.json); do
-    IP=$(jq -r --arg i "$i" '.[] | select(.Name == $i) | .PublicIpAddress' /tmp/nodes.json)
-    LINE="$i ansible_host=$IP ansible_user=ubuntu ansible_python_interpreter=/usr/bin/python3 ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
-    echo $LINE >> /tmp/project.ini
+for i in $(jq -r '.[].Name' $NODES_FILE); do
+    IP=$(jq -r --arg i "$i" '.[] | select(.Name == $i) | .PublicIpAddress' $NODES_FILE)
+    LINE="$i ansible_host=$IP ansible_user=$USER ansible_ssh_private_key_file=$KEY_PATH ansible_python_interpreter=/usr/bin/python3 ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
+    echo $LINE >> $INI_PATH
 done
 
-echo -e "\n" >> /tmp/project.ini
+echo -e "\n" >> $INI_PATH
 
 # Create [kube_masters] Group
-echo "[kube_masters]" >> /tmp/project.ini
+echo "[kube_masters]" >> $INI_PATH
 
-IP=$(jq -r --arg i "$i" '.[] | select(.Name == "k8s-node-1") | .PublicIpAddress' /tmp/nodes.json)
-LINE="k8s-node-1 ansible_host=$IP ansible_user=ubuntu ansible_python_interpreter=/usr/bin/python3 ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
-echo $LINE >> /tmp/project.ini
+IP=$(jq -r --arg i "$i" '.[] | select(.Name == "k8s-node-1") | .PublicIpAddress' $NODES_FILE)
+LINE="k8s-node-1 ansible_host=$IP ansible_user=$USER ansible_ssh_private_key_file=$KEY_PATH ansible_python_interpreter=/usr/bin/python3 ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
+echo $LINE >> $INI_PATH
 
-echo -e "\n" >> /tmp/project.ini
+echo -e "\n" >> $INI_PATH
 
 # Create [kube_workers] Group
-echo "[kube_workers]" >> /tmp/project.ini
+echo "[kube_workers]" >> $INI_PATH
 
-for i in $(jq -r '.[].Name' /tmp/nodes.json | grep -v k8s-node-1); do
-    IP=$(jq -r --arg i "$i" '.[] | select(.Name == $i) | .PublicIpAddress' /tmp/nodes.json)
-    LINE="$i ansible_host=$IP ansible_user=ubuntu ansible_python_interpreter=/usr/bin/python3 ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
-    echo $LINE >> /tmp/project.ini
+for i in $(jq -r '.[].Name' $NODES_FILE | grep -v k8s-node-1); do
+    IP=$(jq -r --arg i "$i" '.[] | select(.Name == $i) | .PublicIpAddress' $NODES_FILE)
+    LINE="$i ansible_host=$IP ansible_user=$USER ansible_ssh_private_key_file=$KEY_PATH ansible_python_interpreter=/usr/bin/python3 ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
+    echo $LINE >> $INI_PATH
 done
