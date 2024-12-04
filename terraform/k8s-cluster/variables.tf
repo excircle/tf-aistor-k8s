2c09350e9a5dc165e2cd8122331cc35e529715c3
excircle <unixdisciple.us@gmail.com>
2024-12-03 20:31:12 -0800

    k8s-cluster update 2

--- terraform/k8s-cluster/variables.tf
+++ terraform/k8s-cluster/variables.tf
@@ -2,3 +2,9 @@ variable "sshkey" {
   description = "SSH key to use for the EC2 instances"
   type        = string
 }
+
+variable "generate_disk_info" {
+  description = "Generate disk_info.json file"
+  type = bool
+  default = false
+}
