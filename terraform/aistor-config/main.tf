31c192de0c043052bd31fa82da37da496edad884
Alexander Kalaj <unixdisciple.us@gmail.com>
2024-12-04 14:45:36 -0800

    AiStor Apply 7

--- terraform/aistor-config/main.tf
+++ terraform/aistor-config/main.tf
@@ -24,9 +24,9 @@ resource "kubernetes_manifest" "persistent_volume" {
   count = var.disk_count
 
   manifest = yamldecode(templatefile("${path.module}/manifests/persistent-volume.yaml", {
-    aistor_pv_name  = format("%s-%d", var.aistor_pv_name, count.index)
+    aistor_pv_name  = format("%s-%d", var.aistor_pv_name, count.index+1)
     aistor_pv_size  = var.aistor_pv_size
-    aistor_data_dir = format("/mnt/data-%d", count.index+1)
+    aistor_data_dir = format("/mnt/data%d", count.index+1)
     hostnames       = var.hostnames
   }))
 }
