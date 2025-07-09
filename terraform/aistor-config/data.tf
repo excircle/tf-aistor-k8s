1493072e5a1167ff45deac8bc1fa01a16472c19a
Alexander Kalaj <unixdisciple.us@gmail.com>
2025-07-09 15:37:29 -0700

    helm testing destroy 2

--- terraform/aistor-config/data.tf
+++ terraform/aistor-config/data.tf
@@ -1,7 +1,7 @@
 locals {
   pvs = flatten(
     [for h in var.hostnames : 
-        [for i in range(1, var.var.disk_count+1) : "${h}-disk${i}"]
+        [for i in range(1, var.disk_count+1) : "${h}-disk${i}"]
     ]
   )
 }
